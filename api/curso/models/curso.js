2e77de33ccdd561a7190ee3c8cb8822cd73ecdea
Ubuntu <ubuntu@ip-172-31-47-148.us-east-2.compute.internal>
2020-11-26 22:18:29 +0000

    running in dev

---
+++ api/curso/models/curso.js
@@ -0,0 +1,19 @@
+'use strict';
+
+/**
+ * Read the documentation (https://strapi.io/documentation/v3.x/concepts/models.html#lifecycle-hooks)
+ * to customize this model
+ */
+
+const slugify = require('slugify');
+
+module.exports = {
+	lifecycles: {
+		async beforeCreate(data) {
+			data.slug = slugify(data.titulo, {lower: true});
+		},
+		async beforeUpdate(params, data) {
+			data.slug = slugify(data.titulo, {lower: true});
+		},
+	},
+}
--- extensions/users-permissions/config/jwt.js
+++ extensions/users-permissions/config/jwt.js
@@ -1,3 +1,6 @@
 module.exports = {
-  jwtSecret: process.env.JWT_SECRET || '35293560-99ca-4e3b-9939-6d1713a827ed'
+  jwtSecret: process.env.JWT_SECRET || '35293560-99ca-4e3b-9939-6d1713a827ed',
+  jwt: {
+    expiresIn: "365d",
+  }
 };
